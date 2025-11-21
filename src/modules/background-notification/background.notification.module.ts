9e5b1f9d58f98b292b7e41d734fbb7649ab7dce2
deybee97 <deybollar@gmail.com>
2025-11-21 21:27:11 +0100

    remove background gateway

--- src/modules/background-notification/background.notification.module.ts
+++ src/modules/background-notification/background.notification.module.ts
@@ -5,16 +5,11 @@ import { FirebaseModule } from '../firebase/firebase.module';
 import { NotificationsService } from './background.notification.service';
 import { RedisModule } from 'src/redis/redis.module';
 import { GuardsModule } from 'src/common/guards/guards.module';
-import { BackgroundNotificationGateway } from './background.notificarion.gateway';
 import { NotificationsController } from './background.notification.controller';
 
 @Module({
   imports: [FirebaseModule, RedisModule, GuardsModule],
-  providers: [
-    NotificationsService,
-    BackgroundNotificationGateway,
-    NotificationsController,
-  ],
-  exports: [NotificationsService, BackgroundNotificationGateway],
+  providers: [NotificationsService, NotificationsController],
+  exports: [NotificationsService],
 })
 export class BackgroundNotificationsModule {}
